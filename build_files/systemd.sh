cc3ba5d0bce387fd35a0a73f39ccb6cc202d2867
muralidharan <muralidramachandran@gmail.com>
2025-01-22 09:21:19 +0530

    removed udev systemctl restarts. When syetem reboots it will be enabled automatically

--- build_files/systemd.sh
+++ build_files/systemd.sh
@@ -9,7 +9,3 @@ systemctl enable check-sb-key.service
 # brew
 systemctl enable brew-upgrade.timer
 systemctl enable brew-update.timer
-
-# udev rules restart - limit battery charge to 70%
-udevadm control --reload-rules
-systemctl restart upower
