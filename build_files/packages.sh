5fe82f5e76717ee4e644c8d42775ae0d98e371df
muralidharan <muralidramachandran@gmail.com>
2025-01-24 14:49:40 +0530

    optimizing package install and added rpmfusion, non-free codecs

--- build_files/packages.sh
+++ build_files/packages.sh
@@ -8,73 +8,85 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# Cleanup unnecessary apps
-dnf remove -y gnome-tour
-dnf remove -y firefox-langpacks
-dnf remove -y firefox
-dnf remove -y toolbox
+# setup rpmfusion
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# # use these to disable system update modules
-# dnf remove gnome-software-rpm-ostree
-# dnf remove ublue-os-update-services
-# dnf remove podman-docker
+# Cleanup unnecessary apps
+dnf remove -y gnome-tour \
+    firefox \
+    firefox-langpacks \
+    toolbox \
+    ffmpeg-free \
+    libavcodec-free \
+    libavdevice-free \
+    libavfilter-free \
+    libavformat-free \
+    libavutil-free \
+    libpostproc-free \
+    libswresample-free \
+    libswscale-free \
+    mesa-va-drivers
 
 # printer / scanner utils
-dnf install -y epson-inkjet-printer-escpr
-dnf install -y epson-inkjet-printer-escpr2
-dnf install -y printer-driver-brlaser
-dnf install -y simple-scan
-dnf install -y foo2zjs
-dnf install -y hplip
-dnf install -y firewall-config
-
-# utilities
-dnf install -y p7zip
-dnf install -y p7zip-plugins
-dnf install -y wl-clipboard
-dnf install -y wireguard-tools
-dnf install -y veracrypt
-dnf install -y solaar
-dnf install -y git-credential-libsecret
-dnf install -y input-remapper
-dnf install -y lm_sensors
-dnf install -y rclone
-dnf install -y restic
-dnf install -y setools-console
-dnf install -y openssh-askpass
-
-# DE tools
-dnf install -y gnome-shell-extension-system-monitor
-dnf install -y gnome-shell-extension-appindicator
-dnf install -y gnome-shell-extension-blur-my-shell
-dnf install -y gnome-shell-extension-caffeine
-dnf install -y ulauncher
-
-# Development
-dnf install -y code
-dnf install -y python3-pip
-dnf install -y libxcrypt-compat
-dnf install -y distrobox
-dnf install -y ublue-brew
-
-# Terminal tools
-dnf install -y zoxide
-dnf install -y stow
-dnf install -y git
-dnf install -y zsh
-dnf install -y fastfetch
-dnf install -y ghostty
-
-# essentials for homebrew
-dnf install -y gcc
-dnf install -y g++
-dnf install -y make
-dnf install -y automake
-dnf install -y autoconf
-dnf install -y glibc-devel
-dnf install -y libstdc++-devel
-dnf install -y binutils
-dnf install -y kernel-devel
+dnf install -y \
+    ffmpeg \
+    gstreamer1-plugin-libav \
+    gstreamer1-plugins-bad-free-extras \
+    gstreamer1-plugins-bad-freeworld \
+    gstreamer1-plugins-ugly \
+    gstreamer1-vaapi \
+    mesa-va-drivers-freeworld \
+    mesa-vdpau-drivers-freeworld \
+    libva-nvidia-driver \
+    steam-devices \
+    intel-media-driver \
+    libva-intel-driver \
+    p7zip \
+    p7zip-plugins \
+    wl-clipboard \
+    wireguard-tools \
+    veracrypt \
+    solaar \
+    git-credential-libsecret \
+    input-remapper \
+    lm_sensors \
+    rclone \
+    restic \
+    setools-console \
+    openssh-askpass \
+    epson-inkjet-printer-escpr \
+    epson-inkjet-printer-escpr2 \
+    printer-driver-brlaser \
+    simple-scan \
+    foo2zjs \
+    hplip \
+    firewall-config \
+    gnome-shell-extension-system-monitor \
+    gnome-shell-extension-appindicator \
+    gnome-shell-extension-blur-my-shell \
+    gnome-shell-extension-caffeine \
+    ulauncher \
+    code \
+    python3-pip \
+    libxcrypt-compat \
+    distrobox \
+    ublue-brew \
+    zoxide \
+    stow \
+    git \
+    zsh \
+    fastfetch \
+    ghostty \
+    gcc \
+    g++ \
+    make \
+    automake \
+    autoconf \
+    glibc-devel \
+    libstdc++-devel \
+    binutils \
+    kernel-devel
 
 # Cleanup unnecessary apps
 dnf remove -y gnome-extensions-app
