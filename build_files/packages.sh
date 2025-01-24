63fc51ed5f3cdedcf6ef8b99f86c6248ca8df58c
muralidharan <muralidramachandran@gmail.com>
2025-01-24 15:44:41 +0530

    test5

--- build_files/packages.sh
+++ build_files/packages.sh
@@ -13,3 +13,70 @@ dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 
+# Cleanup unnecessary apps
+dnf remove -y gnome-tour \
+    firefox \
+    firefox-langpacks \
+    toolbox
+
+dnf install -y \
+    # ffmpeg \
+    # gstreamer1-plugin-libav \
+    # gstreamer1-plugins-bad-free-extras \
+    # gstreamer1-plugins-bad-freeworld \
+    # gstreamer1-plugins-ugly \
+    # gstreamer1-vaapi \
+    # mesa-va-drivers-freeworld \
+    # mesa-vdpau-drivers-freeworld \
+    # libva-nvidia-driver \
+    # steam-devices \
+    # intel-media-driver \
+    # libva-intel-driver \
+    p7zip \
+    p7zip-plugins \
+    wl-clipboard \
+    wireguard-tools \
+    veracrypt \
+    solaar \
+    git-credential-libsecret \
+    input-remapper \
+    lm_sensors \
+    rclone \
+    restic \
+    setools-console \
+    openssh-askpass \
+    epson-inkjet-printer-escpr \
+    epson-inkjet-printer-escpr2 \
+    printer-driver-brlaser \
+    simple-scan \
+    foo2zjs \
+    hplip \
+    firewall-config \
+    gnome-shell-extension-system-monitor \
+    gnome-shell-extension-appindicator \
+    gnome-shell-extension-blur-my-shell \
+    gnome-shell-extension-caffeine \
+    ulauncher \
+    code \
+    python3-pip \
+    libxcrypt-compat \
+    distrobox \
+    ublue-brew \
+    zoxide \
+    stow \
+    git \
+    zsh \
+    fastfetch \
+    ghostty \
+    gcc \
+    g++ \
+    make \
+    automake \
+    autoconf \
+    glibc-devel \
+    libstdc++-devel \
+    binutils \
+    kernel-devel
+    
+# Cleanup unnecessary apps
+dnf remove -y gnome-extensions-app
