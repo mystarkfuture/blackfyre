fd2fbdd85f1e8b576d91bbe757141fe5db943c03
muralidharan <muralidramachandran@gmail.com>
2025-01-24 15:22:44 +0530

    test4

--- build_files/packages.sh
+++ build_files/packages.sh
@@ -12,6 +12,4 @@ set -ouex pipefail
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# Cleanup unnecessary apps
-dnf remove -y gnome-extensions-app
 
