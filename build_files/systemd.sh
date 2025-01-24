bee149c3e11245b8537da75c1efd343f1c9db9e2
muralidharan <muralidramachandran@gmail.com>
2025-01-24 14:23:29 +0530

    added dconf service and flathub

--- build_files/systemd.sh
+++ build_files/systemd.sh
@@ -9,3 +9,11 @@ systemctl enable check-sb-key.service
 # brew
 systemctl enable brew-upgrade.timer
 systemctl enable brew-update.timer
+
+# dconf for system settings
+systemctl enable dconf-update.service
+
+# replace fedora flatpaks with flathub
+systemctl enable flatpak-add-flathub-repo.service
+systemctl enable flatpak-replace-fedora-apps.service
+systemctl enable flatpak-cleanup.timer
